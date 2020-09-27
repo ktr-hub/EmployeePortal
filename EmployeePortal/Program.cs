aff740c8556f74e65bc0852732bb474d59e2b5a5
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-27 09:53:39 +0530

    [Tirupathi] Add. UC1

--- EmployeePortal/Program.cs
+++ EmployeePortal/Program.cs
@@ -4,9 +4,25 @@ namespace EmployeePortal
 {
     class Program
     {
+        Boolean attendanceCheck()
+        {
+            Random random = new Random();
+            int checker = random.Next(0, 2);
+            if (checker == 1)
+                return true;
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Portal ..!");
+            Program attendanceObject = new Program();
+            if (attendanceObject.attendanceCheck())
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
         }
     }
 }
